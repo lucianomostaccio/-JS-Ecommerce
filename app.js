adff4181675537edbce614da594fdef895826a11
Luciano Mostaccio <lucianomostaccio@gmail.com>
2023-04-06 15:49:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+
+//cart
+// const cartIcon = document.querySelector('#cart-icon');
+// let cartOpen = false;
+
+// cartIcon.addEventListener('click', function() {
+//   const cartItemsEl = document.querySelector('#cart-items');
+
+//   if (!cartOpen) {
+//     // Clear any existing items in the cart
+//     cartItemsEl.innerHTML = '';
+
+//     // Add each item to the cart
+//     cartItems.forEach(function(item) {
+//       const itemEl = document.createElement('div');
+//       itemEl.innerText = item.name;
+//       cartItemsEl.appendChild(itemEl);
+//     });
+
+//     // Show the cart
+//     cartItemsEl.style.display = 'block';
+//     cartOpen = true;
+//   } else {
+//     // Hide the cart
+//     cartItemsEl.style.display = 'none';
+//     cartOpen = false;
+//   }
+// });
